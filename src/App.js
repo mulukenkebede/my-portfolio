7331a61b85e039befdade7ea780770c0ca00c2a6
Mulukenkebede <mule.andisew@gmail.com>
2023-07-05 13:35:42 +0300

    my first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi mule</h1>
+
+    </div>
+  );
+}
+
+export default App;
